99b2253344c37b4efddc561ad91184c6ffaa47c6
Igor Flint <igor@dplrn.com>
2019-06-13 19:42:06 +0300

    restructured tests

---
+++ tests/cli/test_new.py
@@ -0,0 +1,17 @@
+from click.testing import CliRunner
+
+from auger.cli.cli import cli
+
+from .constants import COMMAND_STRUCTURE
+
+
+class TestCliTableOfContents(object):
+
+    def test_minimal_arguments_successfull_creation(self):
+        assert False
+
+    def test_project_with_given_name_already_exists(self):
+        assert False
+
+    def test_nested_project_forbiddegn(self):
+        assert False
