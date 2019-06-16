fc8ca65f9d0ee1966400f4d8b88e0764f3cef9cb
Igor Flint <igor@dplrn.com>
2019-06-16 16:23:24 +0300

    fixed Credentials config
    mocked calls for auth tests
    
    fixed vcr recording
    
    -
    
    -

--- tests/cli/test_auth.py
+++ tests/cli/test_auth.py
@@ -1,9 +1,39 @@
+import logging
+
+import vcr
+from click.testing import CliRunner
+
+from auger.cli.cli import cli
+
+
+my_vcr = vcr.VCR(
+    cassette_library_dir='tests/fixtures/cassettes',
+    filter_headers=['authorization'],
+    record_mode='none',
+    )
+
 class TestAuthCLI():
-    def test_login(self):
-        assert False
+    def setup_method(self):
+        self.runner = CliRunner()
 
-    def test_logout(self):
-        assert False
+    @my_vcr.use_cassette('login.yaml')
+    def test_login(self, caplog):
+        caplog.set_level(logging.INFO)
+        result = self.runner.invoke(cli, ['auth', 'login'], input="test@example.com\nauger\npassword\n")
+        assert caplog.records[-1].message == "You are now logged in on https://app.auger.ai as test@example.com."
 
-    def test_whoami(self):
-        assert False
+    @my_vcr.use_cassette('logout.yaml')
+    def test_logout(self, caplog):
+        caplog.set_level(logging.INFO)
+        result = self.runner.invoke(cli, ['auth', 'login'], input="test@example.com\nauger\npassword\n")
+        result = self.runner.invoke(cli, ['auth', 'logout'])
+        assert result.exit_code == 0
+        assert caplog.records[-1].message == "You are loged out of Auger."
+
+    @my_vcr.use_cassette('whoami.yaml')
+    def test_whoami(self, caplog):
+        caplog.set_level(logging.INFO)
+        result = self.runner.invoke(cli, ['auth', 'login'], input="test@example.com\nauger\npassword\n")
+        result = self.runner.invoke(cli, ['auth', 'whoami'])
+        assert result.exit_code == 0
+        assert caplog.records[-1].message  == "test@example.com auger https://app.auger.ai"
