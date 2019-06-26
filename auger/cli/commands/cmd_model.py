b190da77e1e278412b8735ab4db924cd40665582
Igor Flint <igor@dplrn.com>
2019-06-26 21:57:34 +0300

    model-id required for predict; fixed #17

--- auger/cli/commands/cmd_model.py
+++ auger/cli/commands/cmd_model.py
@@ -44,7 +44,7 @@ def deploy(ctx, model_id, locally):
 @click.argument('filename', required=True, type=click.STRING)
 @click.option('--threshold', '-t', default=None, type=float,
     help='Threshold.')
-@click.option('--model-id', '-m', type=click.STRING, required=False,
+@click.option('--model-id', '-m', type=click.STRING, required=True,
     help='Deployed model id.')
 @click.option('--locally', is_flag=True, default=False,
     help='Predict locally using Docker image to run model.')
