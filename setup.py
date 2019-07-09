4570c6fd4983b61dd157eafd1a7ad53ecec754b4
vlad-dplrn <vlad@dplrn.com>
2019-07-09 17:21:02 +0300

    increased version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import codecs
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = '0.1.7'
+VERSION = '0.1.8'
 
 # Get the long description from the README file
 here = os.path.abspath(os.path.dirname(__file__))
