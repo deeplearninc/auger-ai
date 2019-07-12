01c7c29de996cf368c958e2a61ecb31322accdd7
Igor Flint <igor@dplrn.com>
2019-07-12 22:20:41 +0300

    fixed column order for leaderboard. Now sorted alphabetically, model_id is always the first col

--- auger/cli/utils/formatter.py
+++ auger/cli/utils/formatter.py
@@ -9,7 +9,10 @@ def print_table(log, table_list, headers=None):
 
     col_list = headers
     if not col_list:
-        col_list = list(table_list[0].keys() if table_list else [])
+        keys = sorted(table_list[0].keys())
+        if 'model id' in keys:
+            keys = ['model id'] + [key for key in keys if key != 'model id']
+        col_list = list(keys if table_list else [])
     row_list = [col_list]  # 1st row = header
     for item in table_list:
         row_list.append([str(item.get(col) or '') for col in col_list])
